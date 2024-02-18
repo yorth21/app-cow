116783940bb04577c6cc7fdfab15dcc6b89b5370
Yorth Harbin Ortegon Sanchez <yorth21@gmail.com>
2024-02-18 13:06:49 -0500

    Add menu and logout buttons to Navbar component

--- src/pages/Shared/Layout/components/Navbar.jsx
+++ src/pages/Shared/Layout/components/Navbar.jsx
@@ -1,18 +1,32 @@
+import { FaBars, FaArrowRightToBracket } from 'react-icons/fa6'
 import Breadcrumbs from './Breadcrumbs'
+import useLayoutContext from '../hooks/useLayoutContext'
 
 function Navbar () {
+  const { openMenu, setOpenMenu } = useLayoutContext()
+
   return (
     <nav className='block w-full max-w-full px-0 py-1'>
       <div className='flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center'>
         <div className='capitalize'>
           <Breadcrumbs />
         </div>
-        <div>
-          <button>
-            Menu
+        <div className='flex gap-2 text-xl'>
+          <button
+            className={`
+            ${
+              openMenu ? 'bg-slate-200' : ''
+            } text-center p-2.5 rounded-lg flex items-center transition-all hover:bg-slate-200 xl:hidden`}
+            aria-label='Menu'
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <FaBars />
           </button>
-          <button>
-            Salir
+          <button
+            className='text-center p-2.5 rounded-lg flex items-center transition-all hover:bg-slate-200'
+            aria-label='Cerrar sesion'
+          >
+            <FaArrowRightToBracket />
           </button>
         </div>
       </div>
