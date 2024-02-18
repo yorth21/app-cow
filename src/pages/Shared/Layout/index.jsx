6815df40d74fe4a50c9f77e541817d2351b48c81
Yorth Harbin Ortegon Sanchez <yorth21@gmail.com>
2024-02-18 13:06:33 -0500

    Refactor layout component to use layout context

--- src/pages/Shared/Layout/index.jsx
+++ src/pages/Shared/Layout/index.jsx
@@ -1,19 +1,21 @@
 import { Outlet } from 'react-router-dom'
 import Menu from './components/Menu'
 import Navbar from './components/Navbar'
+import { LayoutProvider } from './context/layoutContext'
 
 function Layout () {
   return (
-    <div className='min-h-screen'>
-      <Menu />
+    <LayoutProvider>
+      <div className='min-h-screen'>
+        <Menu />
 
-      <div className='p-4 xl:ml-80'>
-        <Navbar />
+        <div className='p-4 xl:ml-80'>
+          <Navbar />
 
-        <Outlet />
+          <Outlet />
+        </div>
       </div>
-    </div>
-
+    </LayoutProvider>
   )
 }
 
