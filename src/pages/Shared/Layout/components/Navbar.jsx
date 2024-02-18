faf85814fdb2ead3da07c85d0a6e78e61d6788d9
Yorth Harbin Ortegon Sanchez <yorth21@gmail.com>
2024-02-18 11:47:36 -0500

    Add Breadcrumbs component and update Navbar to use it

--- src/pages/Shared/Layout/components/Navbar.jsx
+++ src/pages/Shared/Layout/components/Navbar.jsx
@@ -1,23 +1,11 @@
-import React from 'react'
+import Breadcrumbs from './Breadcrumbs'
 
 function Navbar () {
   return (
-    <nav className='bg-blue-200 block w-full max-w-full px-0 py-1'>
+    <nav className='block w-full max-w-full px-0 py-1'>
       <div className='flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center'>
         <div className='capitalize'>
-          <nav className='w-max'>
-            <ol className='flex flex-wrap items-center w-full'>
-              <li className='flex item-center antialiased text-sm cursor-pointer'>
-                <a href=''>/</a>
-              </li>
-              <li>
-                <a href=''>home</a>
-              </li>
-            </ol>
-          </nav>
-          <h6 className='block antialiased tracking-normal text-base font-semibold leading-relaxed'>
-            home
-          </h6>
+          <Breadcrumbs />
         </div>
         <div>
           <button>
