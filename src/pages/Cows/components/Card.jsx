f508000c925d99390b692255384b9058ba874611
Yorth Harbin Ortegon Sanchez <yorth21@gmail.com>
2024-02-18 18:56:45 -0500

    Refactor Card component to use dynamic data

--- src/pages/Cows/components/Card.jsx
+++ src/pages/Cows/components/Card.jsx
@@ -1,19 +1,19 @@
-import cowImg from '@/assets/images/cow1.webp'
 import { Link } from 'react-router-dom'
+import { PATHS } from '../../../routes/paths'
 
-function Card () {
+function Card ({ cow }) {
   return (
-    <Link to='/cow/1'>
-      <div className='bg-white shadow-sm rounded-lg p-3'>
+    <Link to={`/${PATHS.COWS}/${cow.id}`}>
+      <div className='bg-white shadow-sm rounded-lg p-3 border border-slate-800/20'>
         <div className='flex items-center gap-6'>
           <img
-            src={cowImg}
+            src={cow.img}
             alt='cow'
-            className='inline-block object-cover object-center rounded-lg shadow-lg w-16 h-16'
+            className='inline-block object-cover object-center rounded-lg w-16 h-16'
           />
           <div>
-            <h5 className='text-xl font-semibold leading-snug'>Lolaasdas</h5>
-            <p className='text-sm'>Id: 1</p>
+            <h5 className='text-xl font-semibold leading-snug'>{cow.name}</h5>
+            <p className='text-slate-600 text-sm'>Id: {cow.id}</p>
           </div>
         </div>
       </div>
