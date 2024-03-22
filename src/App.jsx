7749fbd2ad353b79d03e93a82390bece9ac2064f
Yorth Ortegon <yorth21@gmail.com>
2024-03-22 00:01:08 -0500

    Add AuthProvider to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { HashRouter, Routes } from 'react-router-dom'
 import { routes } from './routes/routes'
 import renderRoutes from './routes'
+import { AuthProvider } from '@/contexts/authContext'
 
 function App () {
   return (
     <HashRouter>
-      <Routes>
-        {renderRoutes(routes)}
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          {renderRoutes(routes)}
+        </Routes>
+      </AuthProvider>
     </HashRouter>
   )
 }
