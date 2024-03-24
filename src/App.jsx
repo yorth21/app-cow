d98a46532d5eb2c0a3775cef415f69116b306fec
Yorth Ortegon <yorth21@gmail.com>
2024-03-24 17:32:02 -0500

    Add AxiosInterceptor to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import { HashRouter, Routes } from 'react-router-dom'
 import { routes } from './routes/routes'
 import renderRoutes from './routes'
 import { AuthProvider } from '@/contexts/authContext'
+import { AxiosInterceptor } from './interceptors/axios.interceptor'
 
 function App () {
+  AxiosInterceptor()
+
   return (
     <HashRouter>
       <AuthProvider>
