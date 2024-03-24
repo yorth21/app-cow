32ffead39a4e697e74fbf2d4ffd93c1712016400
Yorth Ortegon <yorth21@gmail.com>
2024-03-24 17:32:46 -0500

    Add reconnect functionality

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,13 +1,33 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import useLocalStorage from '@/hooks/useLocalStorage'
-import { authLogin } from '@/services/auth.service'
+import { authLogin, authReconnect } from '@/services/auth.service'
+import { useNavigate } from 'react-router-dom'
 
 const AuthContext = createContext()
 
 function AuthProvider ({ children }) {
+  const navigate = useNavigate()
   const [token, setToken] = useLocalStorage('token', '')
   const [user, setUser] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    if (token) reconnect()
+  }, [])
+
+  const reconnect = async () => {
+    setLoading(true)
+    try {
+      const res = await authReconnect()
+      setUser(res.data.username)
+      setToken(res.data.token)
+    } catch (error) {
+      logout()
+      console.error('Error reconnecting user.')
+    } finally {
+      setLoading(false)
+    }
+  }
 
   const login = async (credentials) => {
     setLoading(true)
@@ -15,8 +35,9 @@ function AuthProvider ({ children }) {
       const res = await authLogin(credentials)
       setUser(credentials.username)
       setToken(res.data.token)
+      navigate('/')
     } catch (error) {
-      console.log(error)
+      console.log('Error logging in.')
     } finally {
       setLoading(false)
     }
@@ -26,6 +47,8 @@ function AuthProvider ({ children }) {
     setToken('')
     setUser(null)
   }
+
+  if (loading) return <h1>Loading...</h1>
 
   return (
     <AuthContext.Provider
