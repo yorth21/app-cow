fb33761b2500569af0546dfa05d7983a8d9aa424
Yorth Ortegon <yorth21@gmail.com>
2024-03-25 22:17:10 -0500

    Add createUser function to users.service.js

--- src/pages/Users/services/users.service.js
+++ src/pages/Users/services/users.service.js
@@ -5,3 +5,7 @@ const URL_API = import.meta.env.VITE_URL_API
 export async function get () {
   return axios.get(`${URL_API}/users`)
 }
+
+export async function createUser (data) {
+  return axios.post(`${URL_API}/users`, data)
+}
